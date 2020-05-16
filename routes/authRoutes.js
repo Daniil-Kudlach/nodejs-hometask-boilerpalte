efafa2d7846074158037d602163b876f3e002b69
Daniil-Kudlach <daniil.kudlach2018@gmail.com>
2020-05-16 23:32:40 +0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,30 +5,19 @@ const { responseMiddleware } = require('../middlewares/response.middleware');
 const router = Router();
 
 router.post('/login', (req, res, next) => {
-    try {
-        // TODO: Implement login action (get the user if it exist with entered credentials)
-        const email = req.body.email;
-              const password = req.body.password;
-              const user = AuthService.login({ email });
-              if (!user) {
-                res.status(401).send({
-                  error: true,
-                  message: 'Credentials are invalid',
-                });
-              }
-              if (password === user.password) {
-                res.json(user);
-              } else {
-                res.status(401).json({
-                  error: true,
-                  message: 'Credentials are invalid',
-                });
-              }
-    } catch (err) {
-        res.err = err;
-    } finally {
+        try {
+                // TODO: Implement login action (get the user if it exist with entered credentials)
+                const {email, password} = req.body;
+                const data = AuthService.login({email});
+                if (password === data.password) {
+                  res.data = data;
+                } 
+            } catch (err) {
+                res.err = err;
+            } finally {
+                next();
+            }
         next();
-    }
 }, responseMiddleware);
 
 module.exports = router;
