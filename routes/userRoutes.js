b68b800ca45233c1133f583b3155a231044773fe
Daniil-Kudlach <daniil.kudlach2018@gmail.com>
2020-05-16 22:30:38 +0300

    Implement route controllers for user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,5 +6,59 @@ const { responseMiddleware } = require('../middlewares/response.middleware');
 const router = Router();
 
 // TODO: Implement route controllers for user
+router.get('/', (req, res) => {
+  const users = UserService.getUsers();
+  if (users) {
+    res.json(users);
+  } else {
+    res.status(404).json({
+      error: true,
+      message: 'No users in db',
+    });
+  }
+});
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  const foundUser = UserService.search({ id });
+  if (foundUser) {
+    res.json(foundUser);
+  } else {
+    res.status(404).json({
+      error: true,
+      message: 'No user with such id',
+    });
+  }
+});
+
+router.post('/', createUserValid, (req, res) => {
+  const validUser = req.user;
+  if (validUser) {
+    const result = UserService.create(validUser);
+    res.json({result});
+  }
+});
+
+router.put('/:id', updateUserValid, (req, res) => {
+  const id = req.params.id;
+  const userInfo = req.body;
+  const updatedUser = UserService.update(id, userInfo);
+  if (updatedUser) {
+    res.json(updatedUser);
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  const deletedUser = UserService.remove(id);
+  if (deletedUser) {
+    res.json(deletedUser);
+  } else {
+    res.status(404).json({
+      error: true,
+      message: 'No user with such id',
+    });
+  }
+});
 
 module.exports = router;
