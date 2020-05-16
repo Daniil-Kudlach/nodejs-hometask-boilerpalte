110641a6722b47f30d2bdffd3c3418884d1cc14d
Daniil-Kudlach <daniil.kudlach2018@gmail.com>
2020-05-16 22:30:27 +0300

    Implement route controllers for fighter

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -6,5 +6,61 @@ const { createFighterValid, updateFighterValid } = require('../middlewares/fight
 const router = Router();
 
 // TODO: Implement route controllers for fighter
+router.get('/', (req, res) => {
+  const fighters = FighterService.getFighters();
+  if (fighters) {
+    res.json({error:false, body:fighters});
+  } else {
+    res.json({
+      error: true,
+      message: 'Have no fighters',
+    });
+  }
+}, responseMiddleware);
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  const foundFighter = FighterService.search({ id });
+  if (foundFighter) {
+    res.json({error:false, body:foundFighter});
+  } else {
+    res.json({ error: true, message: 'No fighter with such id' });
+  }
+}, responseMiddleware);
+
+router.post('/', createFighterValid, (req, res) => {
+  const validFighter = req;
+  if (validFighter) {
+    const result = UserService.create(validFighter);
+    res.json({result});
+  }
+});
+
+router.put('/:id', updateFighterValid, (req, res) => {
+  const id = req.params.id;
+  const updatedFighter = FighterService.update(id, fighterInfo);
+  if (updatedFighter) {
+    res.json(updatedFighter);
+  } else {
+    res.json({
+      error: true,
+      message: 'No fighter with such id',
+    });
+  }
+});
+
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  const deletedFighter = FighterService.remove(id);
+  if (deletedFighter) {
+    res.json(deletedFighter);
+  } else {
+    res.json({
+      error: true,
+      message: 'No fighter with such id',
+    });
+  }
+}, responseMiddleware);
 
 module.exports = router;
